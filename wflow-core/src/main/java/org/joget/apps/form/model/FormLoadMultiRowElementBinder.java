8d467b065800886a1ee98266ce9f3a0aae5d03f5
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2026-03-06 10:48:10 +0700

    add extends to Form*MultiRowElementBinder

--- wflow-core/src/main/java/org/joget/apps/form/model/FormLoadMultiRowElementBinder.java
+++ wflow-core/src/main/java/org/joget/apps/form/model/FormLoadMultiRowElementBinder.java
@@ -4,6 +4,6 @@ package org.joget.apps.form.model;
  * Used to mark a binder that loads multi row data for an element e.g. Grid.
  * Objects implementing this interface will be selectable in the Form Builder.
  */
-public interface FormLoadMultiRowElementBinder {
+public interface FormLoadMultiRowElementBinder extends FormLoadBinder {
     
 }
--- wflow-core/src/main/java/org/joget/apps/form/model/FormStoreMultiRowElementBinder.java
+++ wflow-core/src/main/java/org/joget/apps/form/model/FormStoreMultiRowElementBinder.java
@@ -4,6 +4,6 @@ package org.joget.apps.form.model;
  * Used to mark a binder that stores multi row data for an element e.g. Grid.
  * Objects implementing this interface will be selectable in the Form Builder.
  */
-public interface FormStoreMultiRowElementBinder {
+public interface FormStoreMultiRowElementBinder extends FormStoreBinder {
     
 }
